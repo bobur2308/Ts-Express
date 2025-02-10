fbf6383de3e8b28742288fbc3c93685afa973a8e
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2025-02-10 08:51:10 +0500

    Update AuthRoutes.ts

--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.post('/register',AuthController.register.bind(AuthController))
 router.post('/login',AuthController.login.bind(AuthController))
 router.post('/refresh',AuthController.refresh.bind(AuthController))
+router.post('/logout',AuthController.logOut.bind(AuthController))
 
 
 export default router;
